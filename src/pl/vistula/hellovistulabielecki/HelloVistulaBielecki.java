f98fbc0c966597298725bb616bb458f10cf25c33
DominikBielecki <bielecki.dominik@gmail.com>
2021-10-04 16:34:34 +0200

    Initial commit

---
+++ src/pl/vistula/hellovistulabielecki/HelloVistulaBielecki.java
@@ -0,0 +1,7 @@
+package pl.vistula.hellovistulabielecki;
+
+public class HelloVistulaBielecki {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula Bielecki");
+    }
+}
