ed52f44af0d535dc0e98be8221e5c6db8a9d073c
DominikBielecki <bielecki.dominik@gmail.com>
2021-10-04 16:38:48 +0200

    Commit 5

--- src/pl/vistula/hellovistulabielecki/HelloVistulaBielecki.java
+++ src/pl/vistula/hellovistulabielecki/HelloVistulaBielecki.java
@@ -4,6 +4,6 @@ public class HelloVistulaBielecki {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Bielecki");
 
-
+        System.out.println("Hello Vistula Bielecki");
     }
 }
